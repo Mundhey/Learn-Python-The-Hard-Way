6cf6e048844c3c14dfbc45ddeed97b4ff4177007
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-05 23:55:00 +0530

    Exercise 1
    A Good First Program

---
+++ Exercise 1/ex1.py
@@ -0,0 +1,8 @@
+print "Hello world"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print "yay printing"
+print "i'd much rather you 'not'"
+print 'I "said" do not touch this'
+print "this prints another line"

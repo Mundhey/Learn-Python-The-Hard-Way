17f543888d4ae1b088a5ec4e064df5412bcdc870
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-06 21:43:23 +0530

    Exercise 4 & Exercise 5

---
+++ Exercise 3/ex3.py
@@ -0,0 +1,24 @@
+print "I will now count my chikens"
+
+print "Hens",25.0+30.0/6.0
+print "Rooster",100.0-25*3%4
+
+print"Now I will count eggs"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "it is true that 3 + 2 < 5 - 7 ?"
+
+print 3 + 2 < 5 - 7
+
+print "What is  3 + 2 ?", 3 + 2
+
+print "what is 5 - 7?",5 - 7
+
+print "Oh thats why it's false"
+print "How about some more"
+
+print "Is is greater?",5>-2
+
+print "Is is greater or equal?",5 >= 7
+
+print "Is it less or equal",5<=7
---
+++ Exercise 5/exercise 5.py
@@ -0,0 +1,17 @@
+my_name="zed shaw"
+my_age=35
+my_height=74
+my_weight=180
+my_eyes="blue"
+my_teeth="white"
+my_hair="brown"
+
+print"lets talk about %s"%my_name
+print "he's %d inches tall"%my_height
+print "he's %d pounds heavy"%my_weight
+print "actually that not that all heavy"
+print "he's got %s eyes and %s hairs"% (my_eyes,my_hair)
+print "he's teeth are usually %s depending on coffee"%my_teeth
+
+#this line is tricky try to get it exactly right
+print "if i add %d,%d and %d I get %d"%(my_age,my_height,my_weight,my_age+my_height+my_weight)

8a387d848a58d0997848caaeccebeba3cab6c92c
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-08 03:39:16 +0530

    Exercise 19

---
+++ Exercise 19/ex19.py
@@ -0,0 +1,22 @@
+def cheese_and_crackers(cheese_count,boxes_of_crackers):
+    print "you have %d cheese !"%cheese_count
+    print "you have %d boxes of crackers"%boxes_of_crackers
+    print "man that's enough for a party"
+    print "get a blanket"
+
+
+print "we can give the function numbers directly"
+
+cheese_and_crackers(20,40)
+
+print "or we can use varibles from our script"
+
+amount_of_cheese=20
+boxes_crackes=50
+
+cheese_and_crackers(amount_of_cheese,boxes_crackes)
+
+print "we can also do math in sode brackets"
+
+cheese_and_crackers(40+60,68+80)
+

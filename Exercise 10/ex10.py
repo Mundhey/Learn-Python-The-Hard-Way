2b37d47cb4f8fb9c320e8f2f01dc21b8124e3655
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-07 08:24:49 +0530

    Exercise 10

---
+++ Exercise 10/ex10.py
@@ -0,0 +1,20 @@
+tabby_cat="\t I am tabbed in"
+persian_cat="I'm split\non line"
+backslash_cat="I'm \\ a \\ cat"
+
+fat_cat="""
+I'll do a list
+\t* Cat food\a
+\t* fishes\a
+\t* catnip\n\t* Grass\a
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+
+while True:
+ for i in ["/","- ","|","\\","|"]:
+  print "%s\r" % i,

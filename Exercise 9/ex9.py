5f853f22d0cbca9aa9c3d77554f1cc372a38d2b3
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-07 08:10:25 +0530

    Exercise 9

---
+++ Exercise 9/ex9.py
@@ -0,0 +1,14 @@
+#Here's some new strange stuff,remember type it exactly
+
+days="mon tue wed thu fri sat sun"
+months="jan\nfeb\nmar\napr\nmay\njun\njul\naug\nsep\noct\nnov\ndec"
+
+print "here are days",days
+print "here are months",months
+
+print """  
+theres something going on here
+with three double quotes
+we will be able to type as much as we like
+even 4 lines if we want or 5,or 6
+"""

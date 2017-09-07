630506813fefcab1d727c2dd5bbedc2a1ed22d8d
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-07 14:10:23 +0530

    Exercise 13

---
+++ Exercise 13/ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+script,first,second,third=argv
+
+print "the script is called",argv
+print"the first vatiable is",first
+print"the second variable is",second
+print"the third variable is",third

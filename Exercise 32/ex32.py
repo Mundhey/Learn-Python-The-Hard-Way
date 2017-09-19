1661acf6805b78a34d1cc3ca336913482cded4b6
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-20 01:19:24 +0530

    Exercise 32

---
+++ Exercise 32/ex32.py
@@ -0,0 +1,24 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1,'pennies',2,'dimes',3,'quarters']
+
+for num in the_count:
+    print "the count is %d"%(num)
+
+for fruit in fruits:
+    print "The fruit is %s"%fruit
+
+for i in change:
+    print "I got %r"%i
+
+element=[]
+
+for i in range(0,6):
+    print "adding %d in list"%i
+    element.append(i)
+
+for i in element:
+    print "Element was %d"%i
+
+
+

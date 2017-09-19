22c4cddd647ef165a09be84e5762803a2a61c245
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-19 20:07:13 +0530

    Exercise 29

---
+++ Exercise 29/ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 40
+dogs = 5
+
+if people < cats:
+    print "too many cats world is doomed"
+
+if people > cats:
+    print "not many cats world is saved"
+
+if people < dogs:
+    print"the world is drooled on"
+
+if people > dogs:
+    print"the world is dry"
+
+dogs += 5
+
+if people >= dogs:
+    print"people are greater than or equal to dogs"
+if people <= dogs:
+    print"people are less than equal to dogs"
+
+if people == dogs:
+    print "people are dogs"
+
